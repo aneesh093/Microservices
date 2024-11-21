00ccbfe0c87e7d4212cf5fe37a831251ce42a471
Aneesh Mathai <43348837+aneesh093@users.noreply.github.com>
2024-11-21 21:49:49 +0530

    Added API Documentation - Swagger Integration

--- accounts/src/main/java/com/talkyteck/accounts/controller/AccountController.java
+++ accounts/src/main/java/com/talkyteck/accounts/controller/AccountController.java
@@ -4,6 +4,10 @@ import com.talkyteck.accounts.constants.AccountConstants;
 import com.talkyteck.accounts.dto.CustomerDTO;
 import com.talkyteck.accounts.dto.ResponseDTO;
 import com.talkyteck.accounts.service.AccountService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
@@ -11,6 +15,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "Accounts CRUD API", description = "Accounts APIs for CRUD")
 @RestController
 @RequestMapping(path = "/api")
 @AllArgsConstructor
@@ -18,6 +23,11 @@ public class AccountController {
 
     private AccountService accountService;
 
+    @Operation(
+            summary = "Create a new customer",
+            description = "This API is used to create a new customer"
+    )
+    @ApiResponse(responseCode = "201", description = "Created")
     @PostMapping("/createAccount")
     public ResponseEntity<ResponseDTO> createAccount(@Valid @RequestBody CustomerDTO customer) {
         accountService.createCustomer(customer);
@@ -26,6 +36,10 @@ public class AccountController {
                 .body(new ResponseDTO(AccountConstants.STATUS_201, AccountConstants.MESSAGE_201));
     }
 
+    @Operation(
+            summary = "Get customer details",
+            description = "This API is used to get customer details"
+    )
     @GetMapping("/getCustomer")
     public ResponseEntity<CustomerDTO> getCustomer(@RequestParam
                                                        @Pattern(regexp = "(^$|[0-9]{10})",message = "Mobile number is not valid")
@@ -36,6 +50,14 @@ public class AccountController {
                 .body(customerDTO);
     }
 
+    @Operation(
+            summary = "Update customer details",
+            description = "This API is used to update customer details"
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Updated"),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error")}
+    )
     @PutMapping("/updateCustomer")
     public ResponseEntity<ResponseDTO> updateCustomer(@Valid @RequestBody CustomerDTO customer) {
         boolean isUpdated = accountService.updateCustomer(customer);
@@ -50,7 +72,14 @@ public class AccountController {
         }
     }
 
-
+@Operation(
+        summary = "Delete customer details",
+        description = "This API is used to delete customer details"
+)
+@ApiResponses({
+        @ApiResponse(responseCode = "200", description = "Deleted"),
+        @ApiResponse(responseCode = "500", description = "Internal Server Error")}
+)
     @DeleteMapping("/deleteCustomer")
     public ResponseEntity<ResponseDTO> deleteCustomer(@RequestParam
                                                           @Pattern(regexp = "(^$|[0-9]{10})",message = "Mobile number is not valid")
